4e0fe217315c1378bf23747de90bab2ffc745a75
fidanmardanli <fidannm@code.edu.az>
2022-07-06 02:47:08 +0400

    Exercises and practises

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckRole(0);
+
+        }
+
+        static void CheckRole(int roleId)
+        {
+            switch (roleId)
+            {
+                case (int)roleId.SuperAdmin:
+                    Console.WriteLine("Super Admin");
+                    break;
+                case (int)roleId.Admin:
+                    Console.WriteLine("Admin");
+                    break;
+                case (int)roleId.Member:
+                    Console.WriteLine("Member");
+                    break;
+                default: 
+                    Console.WriteLine("Not found role id");
+                    break;
+
+            }
+        }
+
+        public enum roleId
+        {
+            SuperAdmin,
+            Member,
+            Admin
+        }
+    }
+}
---
+++ Params/Params/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        public static void GetElements(params int[] nums)
+        {
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            GetElements(1, 2, 3, 4, 5, 6, 7);
+            int[] arr = { 1, 2, 6, 7 };
+            Sum(arr);
+        }
+
+        static void Sum(int[] arr)
+        {
+
+        }
+
+    }
+}
